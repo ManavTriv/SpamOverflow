68a79383f9bbbb9d5d097591ef178e6cae4372a4
Manav <manav@ManavsLaptop>
2024-03-11 20:22:01 +1000

    Added base functionality for database

--- spamoverflow/__init__.py
+++ spamoverflow/__init__.py
@@ -1,10 +1,26 @@
 from os import environ
 from flask import Flask
- 
-def create_app(config_overrides=None): 
-   app = Flask(__name__) 
- 
-   from spamoverflow.views.routes import api 
-   app.register_blueprint(api) 
+from flask_sqlalchemy import SQLAlchemy
 
-   return app
+def create_app(config_overrides=None):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite")
+    if config_overrides: 
+        app.config.update(config_overrides)
+
+    # Load the models
+    from spamoverflow.models import db
+    from spamoverflow.models.todo import Todo
+    db.init_app(app)
+
+    # Create the database tables
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    
+    # Register the blueprints
+    from spamoverflow.views.routes import api
+    app.register_blueprint(api)
+
+    return app
