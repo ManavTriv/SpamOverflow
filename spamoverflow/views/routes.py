ef969bba3dc4d863a06c1a5b40d0f84426f68edf
Manav Trivedi <trivedimanav2003@gmail.com>
2024-03-15 21:44:23 +1000

    Added get emails routes

--- spamoverflow/views/routes.py
+++ spamoverflow/views/routes.py
@@ -17,11 +17,50 @@ binary_name = 'spamhammer.exe'
 binary_path = os.path.join(grandparent_directory, binary_name)
  
 api = Blueprint('api', __name__, url_prefix='/api/v1') 
- 
-@api.route('/health') 
-def health():
-    """Return a status of 'ok' if the server is running and listening to request"""
-    return jsonify({"status": "ok"})
+
+@api.route('/customers/<string:customer_id>/emails/<string:id>', methods=['GET'])
+def get_email(customer_id, id):
+    email = Email.query.filter_by(customer_id=customer_id, id=id).first()
+    return jsonify(email.to_dict()), 201
+
+@api.route('/customers/<string:customer_id>/emails', methods=['GET'])
+def get_emails(customer_id):
+
+    # Returns only this many results, 0 < limit <= 1000. Default is 100.
+    limit = min(int(request.args.get('limit', 100)), 1000) 
+    # Skip this many results before returning, 0 <= offset. Default is 0.
+    offset = max(int(request.args.get('offset', 0)), 0) 
+    # Only return emails submitted from this date. The date should be in RFC3339 format.
+    start= request.args.get('start')
+    # Only return emails submitted before this date. The date should be in RFC3339 format.
+    end = request.args.get('end') 
+    # Only return emails submitted from this email address. The email address should be in the format of user@domain.
+    email_from = request.args.get('from') 
+    # Only return emails submitted to this email address. The email address should be in the format of user@domain.
+    to = request.args.get('to') 
+    # Only return emails that have been flagged as malicious.
+    only_malicious = request.args.get('only_malicious', type=bool)
+
+    query = Email.query.filter_by(customer_id=customer_id)
+    if start:
+        query = query.filter(Email.created_at >= start)
+    if end:
+        query = query.filter(Email.created_at <= end)
+    if email_from:
+        query = query.filter(Email.email_from == email_from)
+    if  to:
+        query = query.filter(Email.to == to)
+    if only_malicious is not None:
+        query = query.filter(Email.malicious == True)
+
+    query = query.limit(limit).offset(offset)
+    emails = query.all()
+    email_list = []
+    
+    for email in emails:
+        email_list.append(email.to_dict())
+    
+    return email_list, 201
 
 @api.route('/customers/<string:customer_id>/emails', methods=['POST'])
 def create_email(customer_id):
@@ -78,3 +117,8 @@ def create_email(customer_id):
     db.session.add(email) 
     db.session.commit() 
     return jsonify(email.to_dict()), 201
+
+@api.route('/health') 
+def health():
+    """Return a status of 'ok' if the server is running and listening to request"""
+    return jsonify({"status": "ok"})
