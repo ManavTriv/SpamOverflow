550555bde31d41405126a1032f65c6a2cf5512c2
Manav Trivedi <trivedimanav2003@gmail.com>
2024-03-22 19:45:36 +1000

    Fixed offset

--- spamoverflow/views/routes.py
+++ spamoverflow/views/routes.py
@@ -34,7 +34,7 @@ def get_emails(customer_id):
 
         try:
             limit = int(request.args.get("limit", 100))
-            offset = int(request.args.get("offset", 100))
+            offset = int(request.args.get("offset", 0))
         except:
             return jsonify({'error': 'Invalid query parameters'}), 400
     
