8a5fbbad0f4057c7947c6bcca543846f8c3ca99a
Manav Trivedi <trivedimanav2003@gmail.com>
2024-04-29 14:30:34 +1000

    refactor naming

--- spamoverflow/tasks/spamscan.py
+++ spamoverflow/tasks/spamscan.py
@@ -8,5 +8,5 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 celery.conf.task_default_queue = os.environ.get("CELERY_DEFAULT_QUEUE", "ical") 
  
 @celery.task(name="ical") 
-def create_ical(tasks): 
+def create_spamscan(tasks): 
    return "Hello World"
